e77ca5a9d5c2d6595c14ad46239ee96993551cbb
la-kr <la-kr@LAPTOP-QMO0GHTF>
2022-09-07 21:58:04 +0200

    new Exception

---
+++ Dionysos/CustomExceptions/InvalidDataException.cs
@@ -0,0 +1,21 @@
+namespace Dionysos.CustomExceptions;
+
+public class InvalidDataException : Exception
+{
+    public InvalidDataException()
+    {
+        
+    }
+
+    public InvalidDataException(string message)
+    : base(message)
+    {
+        
+    }
+
+    public InvalidDataException(string message, Exception inner)
+    : base(message, inner)
+    {
+        
+    }
+}
