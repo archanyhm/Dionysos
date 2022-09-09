e320bcac438c9d2fa1dfd6ba9a06c65237dcacb6
Philipp Micke <archanyhm@gmail.com>
2022-09-09 09:44:22 +0200

    Add database update

---
+++ Dionysos/Migrations/20220909074411_UpdateTableName.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dionysos.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Vendor_VendorId",
+                table: "Articles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vendor",
+                table: "Vendor");
+
+            migrationBuilder.RenameTable(
+                name: "Vendor",
+                newName: "Vendors");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vendors",
+                table: "Vendors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Vendors_VendorId",
+                table: "Articles",
+                column: "VendorId",
+                principalTable: "Vendors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Vendors_VendorId",
+                table: "Articles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vendors",
+                table: "Vendors");
+
+            migrationBuilder.RenameTable(
+                name: "Vendors",
+                newName: "Vendor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vendor",
+                table: "Vendor",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Vendor_VendorId",
+                table: "Articles",
+                column: "VendorId",
+                principalTable: "Vendor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
